bd75321f10280f7e0a08cb5f550ffb3e0a68aae9
Tuan Tu <nhatbang@gmail.com>
2015-03-25 20:15:28 +0700

    --- Commit Hello World ---

---
+++ src/main/java/com/icoding/fw/configuration/DispatcherConfig.java
@@ -0,0 +1,23 @@
+package com.icoding.fw.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+@ComponentScan("com.icoding.fw")
+@EnableWebMvc
+public class DispatcherConfig {
+
+	@Bean
+	public UrlBasedViewResolver setupViewResolver() {
+		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+		resolver.setPrefix("/views");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		return resolver;
+	}
+}
